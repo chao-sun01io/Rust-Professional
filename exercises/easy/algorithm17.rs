6acb83a4fc7b6f4ad67b14a2bb2068a31d3e935b
chao-sun01io <sde.chaosun@gmail.com>
2025-02-24 23:11:21 +0000

    Complete algorithm17

--- exercises/easy/algorithm17.rs
+++ exercises/easy/algorithm17.rs
@@ -25,7 +25,7 @@ pub fn intersection(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> Vec<i32> {
     let set1: HashSet<i32> = nums1.into_iter().collect();
     let set2: HashSet<i32> = nums2.into_iter().collect();
 
-    let mut intersection: Vec<i32> = set1.intersection(&set2).cloned().into_iter().collect();
+    let mut intersection: Vec<i32> = set1.intersection(&set2).cloned().collect();
     intersection.sort_unstable();
     intersection
 }
