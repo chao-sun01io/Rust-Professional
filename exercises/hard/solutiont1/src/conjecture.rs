61be71b714e49cbea641b559d0f2facd9d32c6be
chao-sun01io <sde.chaosun@gmail.com>
2025-03-01 15:37:03 +0000

    Update: hard

--- exercises/hard/solutiont1/src/conjecture.rs
+++ exercises/hard/solutiont1/src/conjecture.rs
@@ -1,3 +1,65 @@
 pub fn goldbach_conjecture() -> String {
-    todo!()
+    let mut primes: Vec<u64> =  Vec::new();
+    let mut result: Vec<String> = Vec::new();
+    for i in 2..10000u64{
+        if is_prime(i) {
+            // println!("Prime {}", i);
+            primes.push(i);
+            continue;
+        }
+
+        if is_odd(i){ // odd composite number
+            let mut flag: bool = false;
+            for p in &primes{
+                let v = i - p;
+                if !is_odd(v) && is_square(v / 2) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if !flag {
+                result.push(i.to_string());
+            }
+        }
+
+        if result.len() == 2 {
+            break;
+        }
+    }
+
+    result.join(",")
+}
+
+fn is_odd(n: u64) -> bool{
+    return n % 2 == 1;
+}
+
+fn is_square(n: u64) -> bool {
+    let root = (n as f64).sqrt() as u64;
+    root * root == n
+}
+
+fn is_prime(n: u64) -> bool{
+    if n <= 1 {
+        return false;
+    }
+
+    if n <= 3 {
+        return true;
+    }
+
+    if n % 2 == 0 || n % 3 == 0 {
+        return false;
+    }
+
+    let mut i = 5;
+    while i * i <= n {
+        if n % i == 0 || n % (i + 2) == 0 {
+            return false;
+        }
+        i += 6;
+    }
+
+    true
 }
--- exercises/hard/solutiont2/src/prime_factor.rs
+++ exercises/hard/solutiont2/src/prime_factor.rs
@@ -1,3 +1,35 @@
-pub fn find_max_prime_factor(number: u128) -> u128 {
-    todo!()
+pub fn find_max_prime_factor(mut number: u128) -> u128 {
+    let mut max_prime_factor = 1;
+
+    // Divide by 2 until number is odd
+    while number % 2 == 0 {
+        max_prime_factor = 2;
+        number /= 2;
+    }
+
+    while number % 3 == 0 {
+        max_prime_factor = 3;
+        number /= 3;
+    }
+
+    // Iterate from 3 to sqrt(number) with step 2
+    let mut i = 5;
+    while i * i <= number {
+        while number % i == 0 {
+            max_prime_factor = i;
+            number /= i;
+        }
+        while number % (i + 2) == 0 {
+            max_prime_factor = (i + 2);
+            number /= (i + 2);
+        }
+        i += 6;
+    }
+
+    // If number is greater than 2, it is a prime number
+    if number > 2 {
+        max_prime_factor = number;
+    }
+
+    max_prime_factor
 }
